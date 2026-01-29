2f652d20d5caa58dae35ad554261316a110a4e59
Corno Schraverus <corno@schraverus.com>
2026-01-29 18:26:59 +0100

    pdt: ...

--- pub/src/interface/generated/liana/schemas/deserialize_parse_tree/data.ts
+++ pub/src/interface/generated/liana/schemas/deserialize_parse_tree/data.ts
@@ -1,8 +1,6 @@
 
 import * as _pi from "pareto-core/dist/interface"
 
-import * as i__location from "../../generic/location"
-
 import * as i__imports_parse_tree from "../parse_tree/data"
 
 import * as i__imports_token from "../token/data"
