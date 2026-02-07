9b38d399d819f5b75d702e1e86e31f2996b99780
Corno Schraverus <corno@schraverus.com>
2026-02-07 21:02:15 +0100

    pdt: ...

--- pub/src/implementation/manual/schemas/deserialize_parse_tree/transformers/fountain_pen.ts
+++ pub/src/implementation/manual/schemas/deserialize_parse_tree/transformers/fountain_pen.ts
@@ -1,7 +1,6 @@
 import * as _p from 'pareto-core/dist/assign'
 import * as _pi from 'pareto-core/dist/interface'
 import _p_list_build_deprecated from 'pareto-core/dist/_p_list_build_deprecated'
-import _p_text_from_list from 'pareto-core/dist/_p_text_from_list'
 import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 import _p_unreachable_code_path from 'pareto-core/dist/_p_unreachable_code_path'
 
